55ba065e1f65470c8a30b324ecb4f5f246eb3aec
Justin Ng <justinng123409@yahoo.com>
2020-11-21 03:41:50 -0800

    initial init

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+//const connectDB = require('./config/db');
+const path = require('path');//core nodejs module to manipulate paths
+
+const app = express();
+
+//connect db
+//connectDB();
+
+//init middleware, allows user.js to get data, ie use req.body
+app.get('/', (req, res) => res.send('test'));
+//app.use(express.json({ extended: false }));
+
+//app.get('/', (req, res) => res.send('API Running'));
+
+//define routes
+// app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/profile', require('./routes/api/profile'));
+// app.use('/api/posts', require('./routes/api/posts'));
+
+//serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//     //set static folder
+//     app.use(express.static('client/build'));
+//     //serve html file
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     })
+
+
+// }
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
