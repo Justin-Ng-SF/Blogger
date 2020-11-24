f798713d9dcbf9bdbc6a39166291cecaba50b358
Justin Ng <justinng123409@yahoo.com>
2020-11-24 14:41:27 -0800

    added get user route

--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../../middleware/auth')
 
+const User = require('../../../models/user')
 
-router.get('/', (req, res) => res.send('Auth route'));
+//@route get api/auth
+//@desc 
+//@access public
+router.get('/', auth, async (req, res)=>{
+    try {
+        //select('-password') removes user.password from user object
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Auth Error');
+    }
+});
 
 module.exports = router;
