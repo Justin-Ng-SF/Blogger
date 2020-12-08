cd162880d4ec05306de588fc353914a79e93b135
Justin Ng <justinng123409@yahoo.com>
2020-12-07 18:10:49 -0800

    added login/register and blog

--- server.js
+++ server.js
@@ -12,11 +12,17 @@ connectDB();
 //init middleware for parse
 app.use(express.json({extended: false}))
 
+//tells html files to go to /assets to get tags (ie script or link)
+app.use(express.static(__dirname + '/assets'));
 //pages
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/assets/html/index.html'));
+    res.sendFile(path.join(__dirname + '/assets/html/login.html'));
+});
+app.get('/blogs', function(req, res) {
+    res.sendFile(path.join(__dirname + '/assets/html/displayBlogs.html'));
 });
 
+app.get('/test', (req, res) => res.send('API Running'));
 
 
 
