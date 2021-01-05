8f4d51dcc0acd426db6bf6fa1d3f6ba8674e4d61
Justin Ng <justinng123409@yahoo.com>
2021-01-04 19:34:48 -0800

    added viewing for single blog by id

--- assets/scripts/script.js
+++
@@ -1,8 +0,0 @@
-fetch('/api/blog').then(response => response.json())
-    .then(data => {
-        let firstdata = JSON.stringify(data)
-        //in the html file this is being called in, assigns id='blog' in that html file to this
-        document.getElementById('blogs').innerHTML = `<div>${firstdata}</div>`
-    });
-
-    // document.getElementById('blogs').innerHTML = `hio`
